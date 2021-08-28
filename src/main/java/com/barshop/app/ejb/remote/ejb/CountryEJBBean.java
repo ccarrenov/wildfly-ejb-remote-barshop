34e51294e663d4b674c7d487beaf17cb5f3ee04d
Cristian Carreño <cristian_c_v@hotmail.com>
2021-08-27 23:57:11 -0400

    update ejb remote

--- src/main/java/com/barshop/app/ejb/remote/ejb/CountryEJBBean.java
+++ src/main/java/com/barshop/app/ejb/remote/ejb/CountryEJBBean.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 
+import org.apache.log4j.Logger;
+
 import com.barshop.app.ejb.remote.CountryEJBRemote;
 import com.barshop.app.models.dto.Country;
 
 @Stateless(name = "CountryEJBBean")
 public class CountryEJBBean implements CountryEJBRemote {
+
+    private static final Logger LOGGER = Logger.getLogger(CountryEJBBean.class);
 
     private List<Country> countries;
 
@@ -19,45 +23,54 @@ public class CountryEJBBean implements CountryEJBRemote {
 
     @Override
     public List<Country> findAll() {
+        LOGGER.info("findAll");        
+        LOGGER.info(countries);
         return countries;
     }
 
     @Override
     public void deleteById( long id ) {
+        LOGGER.info("deleteById");
         int position = -1;
-        for (int i = 0; i < countries.size(); i++) {
-            if (countries.get(i).getId() == id) {
+        for (int i = 0; i < this.countries.size(); i++) {
+            if (this.countries.get(i).getId() == id) {
                 position = i;
             }
         }
 
         if (position != -1) {
-            countries.remove(position);
+            this.countries.remove(position);
         }
+        LOGGER.info(this.countries.size());
     }
 
     @Override
     public void save( Country country ) {
-        countries.add(country);
+        LOGGER.info("save");
+        LOGGER.info(this.countries.size());
+        this.countries.add(country);
     }
 
     @Override
     public void update( Country country ) {
+        LOGGER.info("update");        
         int position = -1;
-        for (int i = 0; i < countries.size(); i++) {
-            if (countries.get(i).getId() == country.getId()) {
+        for (int i = 0; i < this.countries.size(); i++) {
+            if (this.countries.get(i).getId() == country.getId()) {
                 position = i;
             }
         }
 
         if (position != -1) {
-            Country updateC = countries.get(position);
+            Country updateC = this.countries.get(position);
             updateC.setName(country.getName());
             updateC.setThreeDigitIso(country.getThreeDigitIso());
             updateC.setTwoDigitIso(country.getTwoDigitIso());
             updateC.setCountryCallingCode(country.getCountryCallingCode());
             updateC.setCountryCode(country.getCountryCode());
         }
+        
+        LOGGER.info(this.countries.size());                
     }
 
 }
--- src/main/java/com/barshop/app/models/dto/DataAccessObject.java
+++ src/main/java/com/barshop/app/models/dto/DataAccessObject.java
@@ -1,6 +1,9 @@
 package com.barshop.app.models.dto;
 
+import java.io.Serializable;
 
-public class DataAccessObject {
+public class DataAccessObject implements Serializable {
+
+    private static final long serialVersionUID = 538546094522075168L;
 
 }
